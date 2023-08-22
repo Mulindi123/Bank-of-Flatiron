1e6e32d1dcb88cfd16c792c97d14a3a6de609247
Medrine Musabi Mulindi <medrine.mulindi@student.moringaschool.com>
2023-08-22 22:53:37 +0300

    Navbar

--- src/App.js
+++ src/App.js
@@ -2,13 +2,16 @@
 import React from 'react';
 import Transaction from './Transaction';
 import './App.css';
+import Navbar from './Navbar';
 
 function App() {
 
 
   return (
     <div className="App">
+      <Navbar />
       <Transaction />
+    
     </div>
   );
 }
---
+++ src/Navbar.js
@@ -0,0 +1,7 @@
+function Navbar(){
+
+    return <nav>
+        <h1>Bank of Fatiron</h1>
+    </nav>
+}
+export default Navbar
